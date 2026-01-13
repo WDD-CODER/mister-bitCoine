fa2abdcda3dc4cf134edc6dad9cca7cc22d89ef9
Your Name <dan.weibren@gmail.com>
2026-01-13 10:43:27 +0200

    feat(logic): add debounced search and reactive filter UI

--- src/app/components/contact-filter/contact-filter.ts
+++ src/app/components/contact-filter/contact-filter.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnChanges, OnInit } from '@angular/core';
+import { ContactService } from '../../services/contact.service';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { FilterBy } from '../../models/filter-by.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'contact-filter',
@@ -6,6 +10,50 @@ import { Component } from '@angular/core';
   templateUrl: './contact-filter.html',
   styleUrl: './contact-filter.scss',
 })
-export class ContactFilter {
+export class ContactFilter implements OnInit {
+
+  contactService = inject(ContactService)
+
+  filter!: FilterBy
+  private filterSubject = new Subject()
+
+
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.contactService.filterBy$
+      .pipe(
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe({
+        next: filterBy => {
+          return this.filter = filterBy
+        },
+        error: err => console.log('Error', err)
+      })
+
+    this.filterSubject
+    .pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      takeUntilDestroyed(this.destroyRef)
+    )
+    .subscribe(() => {
+      console.log('fatchin contacts')
+      
+      this.contactService.setFilterBy(this.filter)
+    })
+  }
+
+
+  onChangeInput({ target }: Event) {
+    const netFilter = target as HTMLInputElement
+    this.filter = { term: netFilter.value }
+  }
+
+
+  onSetFilterBy(filter: FilterBy) {
+    this.filterSubject.next(filter)
+  }
 
 }
