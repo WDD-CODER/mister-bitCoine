ef77e1b1b39ea9f45eec1abd7dc9796376f95492
Your Name <dan.weibren@gmail.com>
2026-01-14 13:33:24 +0200

    feat(router): define routes for home, contacts, dashboard, and details

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { ContactPage } from './pages/contact-page/contact-page';
+import { DashBoard } from './pages/dash-board/dash-board';
+import { PageNotFound } from './omponents/page-not-found/page-not-found';
+import { ContactDetailsPage } from './pages/contact-details-page/contact-details-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'wallet', component: HomePage},
+  {path:'contacts', component: ContactPage},
+  {path:'dash-board', component: DashBoard},
+  {path:'details', component: ContactDetailsPage},
+  {path:'', pathMatch:'full',redirectTo: 'home'},
+  {path:'**', component: PageNotFound},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
