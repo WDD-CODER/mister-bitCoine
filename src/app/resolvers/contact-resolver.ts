5039ee35fd40fd86642679455e8ffbce96e4cec4
Your Name <dan.weibren@gmail.com>
2026-01-18 12:40:31 +0200

    feat(resolver): add contactResolver with integrated loading state and delay

---
+++ src/app/resolvers/contact-resolver.ts
@@ -0,0 +1,18 @@
+import { ResolveFn } from '@angular/router';
+import { Contact } from '../models/contact.model';
+import { inject } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { LoaderService } from '../services/loader-service';
+import { ContactService } from '../services/contact.service';
+import { delay, finalize, tap } from 'rxjs';
+
+export const contactResolver: ResolveFn<Contact> = (route, state) => {
+  const contactService = inject(ContactService)
+  const loaderService = inject(LoaderService)
+  const contactId = route.params['contactId']
+  return contactService.getContactById(contactId).pipe(
+    tap(() => loaderService.onSetIsLoading(true)),
+    delay(1000),
+    finalize(() => loaderService.onSetIsLoading(false)),
+  )
+};
