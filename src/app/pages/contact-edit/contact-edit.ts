6fbee1a733587897c1451c8efeb46dff9bc26e30
Your Name <dan.weibren@gmail.com>
2026-01-18 10:24:44 +0200

    feat(logic): implement reactive form initialization and save handler

--- src/app/pages/contact-edit/contact-edit.ts
+++ src/app/pages/contact-edit/contact-edit.ts
@@ -1,5 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ContactService } from '../../services/contact.service';
+import { Contact } from '../../models/contact.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'contact-edit',
@@ -7,10 +11,59 @@ import { ContactService } from '../../services/contact.service';
   templateUrl: './contact-edit.html',
   styleUrl: './contact-edit.scss',
 })
-export class ContactEdit {
+export class ContactEdit implements OnInit {
 
   contactService = inject(ContactService)
+  route = inject(ActivatedRoute)
+  router = inject(Router)
 
-  contact = this.contactService.getEmptyContact()
-  
+  newContact = this.contactService.getEmptyContact()
+
+
+  private destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+    this.route.params.pipe(
+      map(params => params['contactId']),
+      filter(contactId => contactId),
+      switchMap(contactId => this.contactService.getContactById(contactId)),
+      takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: contact => this.newContact = contact,
+        error: err => console.log('Error', err)
+      })
+
+  }
+
+  onCloseModal() {
+   const modal =  document.querySelector('dialog')
+   console.log("🚀 ~ ContactEdit ~ onCloseModal ~ modal:", modal)
+   modal?.close()
+  }
+
+
+  handleBirthdayChange(str: Date) {
+    console.log("🚀 ~ ContactEdit ~ handelbirthdayChange ~ str:", str)
+    this.newContact.birthday = str
+  }
+
+
+  onSaveContact(ev: SubmitEvent) {
+    if (!this.newContact.email || !this.newContact.name || !this.newContact.phone) {
+      console.log("🚀 ~ ContactEdit ~ onSaveContact ~ this.newContact.email:", this.newContact)
+      return alert('Missing info!')
+    }
+
+    this.contactService.saveContact(this.newContact as Contact)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        error: err => console.log('err', err),
+        complete: this.onBack
+      })
+  }
+
+  onBack = () => {
+    this.router.navigateByUrl('/contacts')
+  }
+
 }
