7b196c107caefa44eafba404b20d273205df7d1c
Your Name <dan.weibren@gmail.com>
2026-01-18 12:40:35 +0200

    refactor(ui): implement null-safe user handling in home page template

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -2,7 +2,7 @@ import { Component, DestroyRef, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
 import { BitcoinService } from '../../services/bitcoin.service';
-import { Observable, switchMap } from 'rxjs';
+import { filter, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'home-page',
@@ -15,8 +15,9 @@ export class HomePage {
   private userService = inject(UserService)
   private bitcoinService = inject(BitcoinService)
 
-  user$: Observable<User> = this.userService.user$
+  user$: Observable<User | null> = this.userService.user$
   btcRate$: Observable<number> = this.user$.pipe(
+    filter((user): user is User => !!user),
     switchMap(user => this.bitcoinService.getRateStream(user.coins))
   ) 
 
