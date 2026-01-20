668a07e565e6d710cb35f554bde29c4f3bd7697b
Your Name <dan.weibren@gmail.com>
2026-01-20 16:16:22 +0200

    feat(ui): implement signup and login toggle on home page

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -5,6 +5,7 @@ import { BitcoinService } from '../../services/bitcoin.service';
 import { filter, Observable, switchMap } from 'rxjs';
 import { ContactService } from '../../services/contact.service';
 import { Contact } from '../../models/contact.model';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'home-page',
@@ -12,34 +13,63 @@ import { Contact } from '../../models/contact.model';
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
+
 export class HomePage implements OnInit {
 
   private contactService = inject(ContactService)
   private userService = inject(UserService)
   private bitcoinService = inject(BitcoinService)
+  private fb = inject(FormBuilder)
 
   date = Date.now() - 1000 * 60 ** 2 * 30
   contacts: Contact[] | null = null
+  userNameInput!: string;
+  singUp: boolean = true
+
+
 
   contacts$: Observable<Contact[]> = this.contactService.contacts$
-  url = 'https://jsonplaceholder.typicode.com/todos/1'
-  
   user$: Observable<User | null> = this.userService.user$
+  // loggedInUser$: Observable<User | null> = this.userService.LoggedInUser$
+
+
+  // url = 'https://jsonplaceholder.typicode.com/todos/1'
+
   btcRate$: Observable<number> = this.user$.pipe(
     filter((user): user is User => !!user),
     switchMap(user => this.bitcoinService.getRateStream(user.coins))
   )
 
-    ngOnInit(): void {
+  ngOnInit(): void {
 
-      this.contacts$.subscribe({
-        next:contacts => {          
-          this.contacts = contacts},
-        error: err => console.log('err', err)
-      })
+    this.contacts$.subscribe({
+      next: contacts => {
+        this.contacts = contacts
+      },
+      error: err => console.log('err', err)
+    })
   }
 
+onSingUpUser({target}: SubmitEvent) {
+console.log("🚀 ~ HomePage ~ onSingUpUser ~ target:", target)
+}
 
+  SetSingUpLogin($event: MouseEvent) {
+    this.singUp = !this.singUp
+  }
+
+  Login(ev: MouseEvent) {
+
+    // const user = localStorage.getItem(this.SINGED_IN_USERS)
+    // console.log("🚀 ~ HomePage ~ Login ~ user:", user)
+  }
+
+  onChangeName() {
+    if (this.contacts) {
+      console.log("🚀 ~ HomePage ~ onChangeName ~  this.contacts[0]:", this.contacts[0])
+      this.contacts[0].name = 'new'
+    }
+  }
   onAddCoins(ev: MouseEvent) {
     this.userService.addCoins(100)
   }
