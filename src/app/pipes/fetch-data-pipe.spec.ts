a7ab1f463d846f92cdfda9bc6e004af601faf38c
Your Name <dan.weibren@gmail.com>
2026-01-20 16:16:57 +0200

    name update

--- src/app/pipes/fetch-data-pipe.spec.ts
+++ src/app/pipes/fetch-data-pipe.spec.ts
@@ -1,8 +1,8 @@
-import { FetchDataPipe } from './fetch-data-pipe';
+import { FetchData } from './fetch-data-pipe';
 
 describe('FetchDataPipe', () => {
   it('create an instance', () => {
-    const pipe = new FetchDataPipe();
+    const pipe = new FetchData();
     expect(pipe).toBeTruthy();
   });
 });
