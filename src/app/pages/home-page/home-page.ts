cd24bb0c8222dd53f8a86c608e08e0447e36e9a2
Your Name <dan.weibren@gmail.com>
2026-01-20 12:33:09 +0200

    refactor(ui): apply dynamic text coloring and integrate pipes into home view

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
-import { Component, DestroyRef, inject } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.model';
 import { BitcoinService } from '../../services/bitcoin.service';
 import { filter, Observable, switchMap } from 'rxjs';
+import { ContactService } from '../../services/contact.service';
+import { Contact } from '../../models/contact.model';
 
 @Component({
   selector: 'home-page',
@@ -10,18 +12,33 @@ import { filter, Observable, switchMap } from 'rxjs';
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
+  private contactService = inject(ContactService)
   private userService = inject(UserService)
   private bitcoinService = inject(BitcoinService)
 
   date = Date.now() - 1000 * 60 ** 2 * 30
+  contacts: Contact[] | null = null
 
+  contacts$: Observable<Contact[]> = this.contactService.contacts$
+  url = 'https://jsonplaceholder.typicode.com/todos/1'
+  
   user$: Observable<User | null> = this.userService.user$
   btcRate$: Observable<number> = this.user$.pipe(
     filter((user): user is User => !!user),
     switchMap(user => this.bitcoinService.getRateStream(user.coins))
   )
+
+    ngOnInit(): void {
+
+      this.contacts$.subscribe({
+        next:contacts => {          
+          this.contacts = contacts},
+        error: err => console.log('err', err)
+      })
+  }
+
 
   onAddCoins(ev: MouseEvent) {
     this.userService.addCoins(100)
