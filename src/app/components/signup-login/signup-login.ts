9827c9e75a62fc4ef71f7df799491d8790882bb0
Your Name <dan.weibren@gmail.com>
2026-01-21 11:34:57 +0200

    feat(ui): implement reactive SignupLogin component with error handling

---
+++ src/app/components/signup-login/signup-login.ts
@@ -0,0 +1,64 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { ContactService } from '../../services/contact.service';
+import { mustContainHash, nameTaken, OnlyEnglishLetters } from '../../costume-validators/contact.validator/contact.validator';
+import { userNameTaken, userNotSignin } from '../../costume-validators/user.validators/user.validator';
+
+const LOGGED_IN_USER = 'signed-users'
+
+@Component({
+  selector: 'signup-login',
+  standalone: false,
+  templateUrl: './signup-login.html',
+  styleUrl: './signup-login.scss',
+})
+export class SignupLogin {
+
+  private fb = inject(FormBuilder)
+  private userService = inject(UserService)
+
+  public userSigninForm!: FormGroup
+  public userLoginForm!: FormGroup;
+  public user: User | null = null
+
+  public singUp: boolean = false
+
+  constructor() {
+    this.userSigninForm = this.fb.group({
+      name: ['', [Validators.required, OnlyEnglishLetters], userNameTaken(this.userService)],
+      email: ['', [Validators.required, mustContainHash]],
+    })
+    this.userLoginForm = this.fb.group({
+      name: ['', [Validators.required, OnlyEnglishLetters], userNotSignin(this.userService)],
+    })
+  }
+
+
+  onSingUpUser(ev: SubmitEvent) {
+    if (this.userSigninForm.invalid) {
+      this.userSigninForm.markAllAsTouched()
+      return
+    }
+    this.userService.setLoggedUser(this.userSigninForm.value)
+  }
+
+  setSingUpLogin($event: MouseEvent) {
+    this.singUp = !this.singUp
+    this.userSigninForm.reset()
+  }
+
+  onLogin(ev: MouseEvent) {
+    console.log("🚀 ~ SignupLogin ~ onLogin ~ this.userSigninForm.invalid:", this.userSigninForm.invalid)
+    if (this.userLoginForm.invalid) {
+      this.userLoginForm.markAllAsTouched()
+      return
+    } 
+    // console.log('variable')
+    
+    // console.log("🚀 ~ SignupLogin ~ onLogin ~ this.userLoginForm.value:", this.userLoginForm.value)
+    this.userService.login(this.userLoginForm.value.name)
+  }
+
+}
