ca1726ffb44cf134258f982a4968e536359961c7
Your Name <dan.weibren@gmail.com>
2026-01-21 11:34:55 +0200

    feat(validator): add userNameTaken and userNotSignin async validators

---
+++ src/app/costume-validators/user.validators/user.validator.ts
@@ -0,0 +1,42 @@
+import { AbstractControl, AsyncValidatorFn } from "@angular/forms";
+import { map, switchMap, take, timer } from "rxjs";
+import { UserService } from "../../services/user.service";
+
+
+export function userNameTaken(userService: UserService): AsyncValidatorFn {
+  return (control: AbstractControl) => {
+    if (!control.value) return timer(0).pipe(map(() => null));
+
+    return timer(1000).pipe(
+      // Switch to the contacts observable from your service
+      switchMap(() => userService.signedUsers$.pipe(
+        take(1),
+        map(users => {
+          const isTaken = users?.some(c => 
+            c.name.toLowerCase() === control.value.toLowerCase()
+          );
+          return isTaken ? { userNameTaken: 'Name is already in use' } : null;
+        })
+      ))
+    );
+  };
+}
+
+export function userNotSignin(userService: UserService): AsyncValidatorFn {
+  return (control: AbstractControl) => {
+    if (!control.value) return timer(0).pipe(map(() => null));
+
+    return timer(1000).pipe(
+      // Switch to the contacts observable from your service
+      switchMap(() => userService.signedUsers$.pipe(
+        take(1),
+        map(users => {
+          const exist = users?.some(c => 
+            c.name.toLowerCase() === control.value.toLowerCase()
+          );
+          return !exist ? { userNotSignin: 'No such user is signed in ' } : null;
+        })
+      ))
+    );
+  };
+}
