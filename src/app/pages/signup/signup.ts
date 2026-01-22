990a5892a8c9da0387045128ecd33850c9707f27
Your Name <dan.weibren@gmail.com>
2026-01-22 14:39:05 +0200

    feat(ui): migrate SignupLogin component to standalone Signup page with 100 coin start

---
+++ src/app/pages/signup/signup.ts
@@ -0,0 +1,73 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { ContactService } from '../../services/contact.service';
+import { mustContainHash, nameTaken, OnlyEnglishLetters } from '../../costume-validators/contact.validator/contact.validator';
+import { userNameTaken, userNotSignin } from '../../costume-validators/user.validators/user.validator';
+const LOGGED_IN_USER = 'signed-users'
+
+@Component({
+  selector: 'signup',
+  standalone: false,
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss',
+})
+export class Signup {
+
+  private fb = inject(FormBuilder)
+  private userService = inject(UserService)
+
+  public userSigninForm!: FormGroup
+  public userLoginForm!: FormGroup;
+  public user: User | null = null
+
+  public Signup: boolean = false
+
+  constructor() {
+    this.userSigninForm = this.fb.group({
+      name: ['', [Validators.required, OnlyEnglishLetters], userNameTaken(this.userService)],
+      email: ['', [Validators.required, mustContainHash]],
+      coins: [100],
+      moves: [[]]
+    })
+    this.userLoginForm = this.fb.group({
+      name: ['', [Validators.required, OnlyEnglishLetters], userNotSignin(this.userService)],
+    })
+  }
+
+
+  onSignupUser(ev: SubmitEvent) {
+    if (this.userSigninForm.invalid) {
+      this.userSigninForm.markAllAsTouched()
+      return
+    }
+    return this.userService.setSignedUser(this.userSigninForm.value)
+    
+  }
+
+  setSignupLogin($event: MouseEvent) {
+    this.Signup = !this.Signup
+
+  this.userSigninForm.reset({
+    name:[''],
+    email:[''],
+    coins:[100],
+    moves:[[]],
+  })
+
+  this.userLoginForm.reset({
+    name:[''],
+  })
+  }
+
+  onLogin(ev: MouseEvent) {
+    if (this.userLoginForm.invalid) {
+      this.userLoginForm.markAllAsTouched()
+      return
+    }
+
+    this.userService.login(this.userLoginForm.value.name)
+  }
+
+}
