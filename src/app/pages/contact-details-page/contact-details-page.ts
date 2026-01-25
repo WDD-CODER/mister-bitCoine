f1ae5c6204027e2f648fab2505782433f2f07881
Your Name <dan.weibren@gmail.com>
2026-01-25 18:36:18 +0200

    feat(contact): display filtered transaction history in details

--- src/app/pages/contact-details-page/contact-details-page.ts
+++ src/app/pages/contact-details-page/contact-details-page.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { ContactService } from '../../services/contact.service';
-import { Observable } from 'rxjs';
+import { combineLatest, filter, map, Observable, tap } from 'rxjs';
 import { Contact } from '../../models/contact.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
@@ -17,11 +17,21 @@ export class ContactDetailsPage implements OnInit {
 
   private contactService = inject(ContactService)
   private userService = inject(UserService)
+  private route = inject(ActivatedRoute)
   private router = inject(Router)
   private cd = inject(ChangeDetectorRef)
 
-  public contact$: Observable<Contact | null> = this.contactService.selectedContact$
+  public contact$: Observable<Contact | null> = this.route.data.pipe(map(data => data['contact']))
+  user$ = this.userService.user$
 
+  public contactMoves$ = combineLatest([this.user$, this.contact$]).pipe(
+    filter(([user]) => !!user),
+    map(([user, contact]) => {
+      console.log("🚀 ~ ContactDetailsPage ~ contact:", contact)
+      console.log("🚀 ~ ContactDetailsPage ~ user:", user)
+      
+     return  user?.moves?.filter(move => move.toId === contact?._id)})
+  )
   public showTransferOption: boolean = false
 
   amountToTransfer: number | null = null;
@@ -41,7 +51,10 @@ export class ContactDetailsPage implements OnInit {
         this.amountToTransfer = null
         this.cd.markForCheck()
       },
-      error: err => console.log('err', err)
+      error: err => {
+        this.amountToTransfer = null
+        console.log('err', err)
+      }
 
     })
   }
