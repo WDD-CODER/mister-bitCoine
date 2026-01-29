82ea19af3025935038b126137e3c737f19dcbdff
Your Name <dan.weibren@gmail.com>
2026-01-29 10:05:20 +0200

    refactor(core): centralized app configuration and clean bootstrap

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { provideRouter } from '@angular/router';
+import { routes } from './app-routing-module'; 
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ApplicationConfig } from '@angular/core';
+
+export const  appConfig: ApplicationConfig = {
+ providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync('animations')
+
+]} 
+// (App, {
+ 
+//     provideBrowserGlobalErrorListeners(),
+//   ]
+// }).catch(err => console.error(err));
+
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing-module'; 
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { App } from './app/app-root/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    provideBrowserGlobalErrorListeners(),
-    provideAnimationsAsync('animations')
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(App, appConfig).catch(err => console.error(err));
 
-// import { platformBrowser } from '@angular/platform-browser';
-// import { AppModule } from './app/app-module';
-
-// platformBrowser().bootstrapModule(AppModule, {
-//   ngZoneEventCoalescing: true,
-// })
-//   .catch(err => console.error(err));
